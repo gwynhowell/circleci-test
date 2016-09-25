65a24177a576f248f148f4f4edf2dd51b48fe4dd
Gwyn Howell <ghowell@appogeehr.com>
2016-09-25 17:59:47 +0100

    add nother random funciton

--- src/util.py
+++ src/util.py
@@ -5,3 +5,6 @@ def some_random_method_that_does_sweet_fa():
     x = random.randint(1, 100)
     y = random.randint(1, 100)
     return x < y
+
+def add_my_numbers_up_dude(number1, number2):
+    return number1 + number2
